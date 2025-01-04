6ffa9f7942509bdea10014f48230ccb6c36f846e
Zoltan KADA <kada.zoli@gmail.com>
2025-01-04 21:51:44 +0100

    .

--- scripts/prepack.js
+++ scripts/prepack.js
@@ -1,10 +1,10 @@
 const shell =require('shelljs');
 shell.rm('-rf', 'dist');
-shell.mkdir('-p','dist/google');
-shell.mkdir('-p','dist/bin');
-shell.cp('-R','protobuf-javascript/google/','dist/');
-shell.cp('-R','protobuf-javascript/google-protobuf.js','dist/');
-shell.cp('-R','protobuf-javascript/LICENSE*','dist/');
-shell.cp('-R','protobuf-javascript/package.json','dist/');
+shell.mkdir('-p','google');
+shell.mkdir('-p','bin');
+shell.cp('-R','protobuf-javascript/google/','.');
+shell.cp('-R','protobuf-javascript/google-protobuf.js','google-protobuf.js');
+// shell.cp('-R','protobuf-javascript/LICENSE*','dist/');
+// shell.cp('-R','protobuf-javascript/package.json','dist/');
 shell.cp('-R','protobuf-javascript/README.md','dist/');
-shell.cp('-R','protobuf-javascript/bazel-bin/generator/protoc-gen-js','dist/bin/');
+shell.cp('-R','protobuf-javascript/bazel-bin/generator/protoc-gen-js','bin/');
